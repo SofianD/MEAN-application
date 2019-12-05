d64922d5c960252f2d3b79afebd741c8eede83e2
SofianD <doual.sofian@hotmail.com>
2019-12-05 14:13:03 +0100

    Add Nodemon. Update server.js and app.js

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "1111",
+      title: "Wsh la street.",
+      content: "WESH LA STREEEEEEEEEEEEEEEET"
+    },
+    {
+      id: "2222",
+      title: "Sisi la street.",
+      content: "SISIII LA STREEEEEEEEEEEEEEEET"
+    }
+  ]
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  })
 });
 
 module.exports = app;
